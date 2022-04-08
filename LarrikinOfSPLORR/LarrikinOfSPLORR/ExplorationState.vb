9703fbb239e673bd1a33ea65dc044cc4dc4a773f
TheGrumpyGameDev <ernestpazera@msn.com>
2022-04-08 15:45:07 -0500

    random doors and walls

--- LarrikinOfSPLORR/LarrikinOfSPLORR/ExplorationState.vb
+++ LarrikinOfSPLORR/LarrikinOfSPLORR/ExplorationState.vb
@@ -1,4 +1,6 @@
-﻿Module ExplorationState
+﻿Imports System.Runtime.CompilerServices
+
+Module ExplorationState
     Const Solid = "X"c
     ReadOnly Walls As New List(Of String) From {
         "XX                                                    XX",
@@ -72,14 +74,25 @@
     End Sub
     Const ViewColumns = 56
     Const ViewRows = 28
-    Private Sub DrawLocation(character As PlayerCharacter)
-        Dim canvas As New Canvas(ViewColumns, ViewRows)
-        For column = 0 To ViewColumns - 1
-            For row = 0 To ViewRows - 1
-                Dim cellColor = If(Doors(row)(column) = Solid, Color.White, Color.Black)
-                canvas.SetPixel(column, row, cellColor)
+    Const LeftColumn = 0
+    Const AheadColumn = 14
+    Const RightColumn = 42
+    <Extension()>
+    Public Sub DrawImage(canvas As Canvas, startColumn As Integer, startRow As Integer, image As List(Of String), solidColor As Color?, emptyColor As Color?)
+        For row = 0 To image.Count - 1
+            For column = 0 To image(row).Length - 1
+                Dim cellColor = If(image(row)(column) = Solid, solidColor, emptyColor)
+                If cellColor.HasValue Then
+                    canvas.SetPixel(column + startColumn, row + startRow, cellColor.Value)
+                End If
             Next
         Next
+    End Sub
+    Private Sub DrawLocation(character As PlayerCharacter)
+        Dim canvas As New Canvas(ViewColumns, ViewRows)
+        canvas.DrawImage(LeftColumn, 0, If(RNG.RollDice("1d2") = 1, LeftWall, LeftDoor), Color.White, Color.Black)
+        canvas.DrawImage(AheadColumn, 0, If(RNG.RollDice("1d2") = 1, AheadWall, AheadDoor), Color.White, Color.Black)
+        canvas.DrawImage(RightColumn, 0, If(RNG.RollDice("1d2") = 1, RightWall, RightDoor), Color.White, Color.Black)
         AnsiConsole.Clear()
         AnsiConsole.Write(canvas)
     End Sub
