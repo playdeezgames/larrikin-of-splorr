7873fd0c3c9015fb23b03d18ef06654f71f9eaf3
TheGrumpyGameDev <ernestpazera@msn.com>
2022-04-08 15:56:32 -0500

    CHEST

---
+++ LarrikinOfSPLORR/LarrikinOfSPLORR/Images/Features.vb
@@ -0,0 +1,16 @@
+﻿Module Features
+    Friend ReadOnly Chest As New List(Of String) From {
+        "            ",
+        "            ",
+        "            ",
+        "  X XXXXX X ",
+        " XX XXXXX XX",
+        " XX XXXXX XX",
+        " XXXXXXXXXXX",
+        "            ",
+        " XXXXXXXXXXX",
+        " XXXX   XXXX",
+        " XXXXX XXXXX",
+        " XXXXXXXXXXX"
+        }
+End Module
